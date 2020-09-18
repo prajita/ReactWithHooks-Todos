2f2fdaac563b6163fa66c5d700dc0701bb18f18e
Prajita <prajitacst@gmail.com>
2020-09-18 23:05:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Todo({ todo, index, completeTodo, deleteTodo }) {
+
+  return (
+    <div
+      style={{ textDecoration: todo.done ? 'line-through' : '' }}
+      className="todo">
+      <span>{index + 1 + ". "}</span>
+
+      {todo.text}
+
+      <span><button onClick={() => completeTodo(index)}>Complete</button></span>
+      <span><button onClick={() => deleteTodo(index)}>X</button></span>
+    </div>
+  )
+
+}
+
+function TodoForm({ addTodo }) {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+  const [value, setValue] = useState('');
+  return (
+
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeHolder="add text here"
+        onChange={e => setValue(e.target.value)}
+
+      ></input>
+    </form>
+  );
+}
 function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      text: "start coding",
+      done: false
+    },
+    {
+      text: "drink warm water",
+      done: false
+    },
+    {
+      text: "eat brunch",
+      done: false
+    },
+    {
+      text: "scedule meeting",
+      done: false
+    },
+    {
+      text: "drink coffee",
+      done: false
+    },
+    {
+      text: "meet friend",
+      done: false
+    },
+    {
+      text: "have snacks",
+      done: false
+    },
+    {
+      text: "set alarm",
+      done: false
+    }
+
+
+  ]);
+
+  const addTodo = text => {
+    const newtodo = [...todos, { text: text, done: false }];
+    setTodos(newtodo);
+  }
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].done = true;
+    setTodos(newTodos);
+  }
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-list">
+        {
+          todos.map((e, i) => (
+            <Todo
+              key={i}
+              index={i}
+              todo={e}
+              completeTodo={completeTodo}
+              deleteTodo={deleteTodo}
+            ></Todo>
+          ))
+        }
+        <TodoForm addTodo={addTodo}></TodoForm>
+      </div>
     </div>
   );
 }
